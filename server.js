071d2c1865d99aa910509342422e3e7d8bc198e0
umang <25umangagarwal@gmail.com>
2021-04-11 00:42:32 +0530

    fix changes

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 const http = require('http');
 const app = require('./app');
-require("dotenv/config");
-
-const Port = process.env.PORT|3000;
 const server = http.createServer(app);
-
-server.listen(Port);
+server.listen(process.env.PORT, '0.0.0.0');
