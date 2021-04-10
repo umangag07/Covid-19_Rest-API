64f0a93279fbd647eb2b3178271bc988b3999a82
umang <25umangagarwal@gmail.com>
2021-04-11 00:15:24 +0530

    corrected spelling mistake

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 require("dotenv/config");
 
-const Port = process.env.Port|3000;
+const Port = process.env.PORT|3000;
 const server = http.createServer(app);
 
 server.listen(Port);
