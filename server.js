97915994f049d9f401388435729e9326f5228eaa
umang <25umangagarwal@gmail.com>
2021-04-11 00:06:58 +0530

    feat: added env variable to port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
+require("dotenv/config");
 
-const Port = 3000;
+const Port = process.env.Port|3000;
 const server = http.createServer(app);
 
 server.listen(Port);
