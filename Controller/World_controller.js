70634997eb9b5a9c3dd2a431b9ad0481f23ed78e
Kriti Thakrar <66572570+KritiThakrar@users.noreply.github.com>
2021-05-05 03:53:33 +0530

    Update World_controller.js

--- Controller/World_controller.js
+++ Controller/World_controller.js
@@ -21,7 +21,6 @@ exports.retriveRequiredData = async (req, res) => {
       unitData.todayRecovered = f.todayRecovered;
       unitData.deaths = f.deaths;
       unitData.todayDeaths = f.todayDeaths;
-      unitData.flag = f.countryInfo.flag;
       unitData.tests = f.tests
       unitData.population = f.population;
       
